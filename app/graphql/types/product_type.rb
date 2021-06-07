b5415b8bb8241b137708b55390a20b7546d3ab80
Uon Liaquat <antelopes06@gmail.com>
2021-06-07 22:28:41 +0500

    Added description field in Product Table

--- app/graphql/types/product_type.rb
+++ app/graphql/types/product_type.rb
@@ -1,10 +1,11 @@
 module Types
   class ProductType < Types::BaseObject
     field :id, ID, null: false
-    field :name, String, null: true
-    field :price, Float, null: true
-    field :stock, Integer, null: true
-    field :image_url, String, null: true
+    field :name, String, null: false
+    field :price, Float, null: false
+    field :stock, Integer, null: false
+    field :image_url, String, null: false
+    field :description, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end

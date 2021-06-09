68eb46b31cc856b26d75c9ac4c970f2d33834d0b
Hamza Noor <hamza.randhawa@deltalabs.io>
2021-06-09 23:17:02 +0500

    Add Cart Table

---
+++ app/models/cart.rb
@@ -0,0 +1,16 @@
+class Cart < ApplicationRecord
+    belongs_to :user
+    has_many :items, dependent: :destroy
+
+    # has_many :products, through: :items
+
+    # # LOGIC
+    # def sub_total
+    #     sum = 0
+    #     self.items.each do |item|
+    #     sum+= item.total_price
+    #     end
+    #     return sum
+    # end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_07_175009) do
+ActiveRecord::Schema.define(version: 2021_06_09_180526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
